4130916719dc8c6261c251a61a524b65e6f86385
winstaredu <contact@winstaredu.com>
2026-06-24 18:45:18 +0600

    Remove package.json content from index.js
    Removed package.json content from index.js.

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-{
-  "name": "winstar-bot",
-  "version": "1.0.0",
-  "description": "WinStar Education Facebook Messenger Chatbot",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "axios": "^1.6.0"
-  }
-}
+
