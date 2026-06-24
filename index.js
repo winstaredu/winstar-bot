16704f2c629bef1f20c58332356c665eef977d1d
winstaredu <contact@winstaredu.com>
2026-06-24 18:29:27 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "winstar-bot",
+  "version": "1.0.0",
+  "description": "WinStar Education Facebook Messenger Chatbot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0"
+  }
+}
